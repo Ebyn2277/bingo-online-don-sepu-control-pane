28e67bb537d4a9aa84d02e52c358ec5dd2c766ac
Brayan Presiga <presigabrayan3@gmai.com>
2025-10-14 11:23:16 -0500

    Add lines' menu to download screenshot and center lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import html2canvas from "html2canvas";
 import "./App.css";
 import { useAuth } from "./hooks/useAuth";
 import { useBingoInfo } from "./hooks/useBingoInfo";
@@ -28,6 +29,33 @@ function App() {
 
   const [selectedLine, setSelectedLine] = useState(null);
   const [selectedUser, setSelectedUser] = useState(null);
+
+  const linesRef = useRef(null);
+  const [screenshotURL, setScreenshotURL] = useState(null);
+
+  const scrollToLines = () => {
+    linesRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  const captureLines = async () => {
+    if (linesRef.current) {
+      const canvas = await html2canvas(linesRef.current, {
+        backgroundColor: "#222",
+        scale: 2,
+      });
+
+      const dataUrl = canvas.toDataURL("image/png");
+
+      setScreenshotURL(dataUrl);
+    }
+  };
+
+  const onClickDownloadScreenshotHandler = () => {
+    setScreenshotURL(null);
+  };
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
@@ -137,7 +165,31 @@ function App() {
               </label>
             </form>
           </div>
-          <ul className="lines">
+          <ul className="lines-menu">
+            <li>
+              <button className="center-lines-button" onClick={scrollToLines}>
+                Centrar
+              </button>
+            </li>
+            <li>
+              <button className="take-screenshot-button" onClick={captureLines}>
+                Capturar pantalla
+              </button>
+            </li>
+            {screenshotURL && (
+              <li className="screenshot-container">
+                <a
+                  href={screenshotURL}
+                  download="element-screenshot.png"
+                  onClick={onClickDownloadScreenshotHandler}
+                >
+                  <h3>Descargar</h3>
+                  <img src={screenshotURL} alt="Captured Element" />
+                </a>
+              </li>
+            )}
+          </ul>
+          <ul className="lines" ref={linesRef}>
             {totalLines &&
               Array.from({ length: totalLines }).map((_, i) => {
                 // Get all purchases for this line
