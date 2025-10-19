3ab5df5b92e9befccdb59eb63ef5fdb065b45b1b
Brayan Presiga <presigabrayan3@gmai.com>
2025-10-19 17:33:16 -0500

    Create Lines component

--- src/ControlPanel.jsx
+++ src/ControlPanel.jsx
@@ -4,6 +4,7 @@ import { useLines } from "./hooks/useLines";
 import "./ControlPanel.css";
 import { BingoInfo } from "./BingoInfo.jsx";
 import { LinesMenu } from "./LinesMenu.jsx";
+import { Lines } from "./Lines.jsx";
 
 export function ControlPanel({ isLoggedIn, logout, getToken }) {
   const token = getToken();
@@ -70,9 +71,6 @@ export function ControlPanel({ isLoggedIn, logout, getToken }) {
     }
   };
 
-  const checkInSelectedPurchases = (purchase) =>
-    selectedPurchases?.some((s) => s.id === purchase.id);
-
   return (
     <>
       <section className="control-panel">
@@ -89,81 +87,15 @@ export function ControlPanel({ isLoggedIn, logout, getToken }) {
           active={active}
           setActive={setActive}
         ></BingoInfo>
-        <LinesMenu linesRef={linesRef}></LinesMenu>
-        <ul className="lines" ref={linesRef}>
-          {totalLines &&
-            Array.from({ length: totalLines }).map((_, i) => {
-              // Get all purchases for this line
-              const linePurchases = lines.filter(
-                (line) => line.line_id === i + 1
-              );
-              return (
-                <li key={i}>
-                  <span>{i + 1}.</span>
-                  <ul className="users">
-                    {Array.from({
-                      length: maxPurchasesPerLine,
-                    }).map((_, j) => {
-                      if (j < linePurchases.length) {
-                        return (
-                          <li
-                            key={j}
-                            className={`${linePurchases[j].state} ${
-                              checkInSelectedPurchases(linePurchases[j])
-                                ? "selected"
-                                : ""
-                            }`}
-                          >
-                            <button
-                              onClick={() => {
-                                if (
-                                  checkInSelectedPurchases(linePurchases[j])
-                                ) {
-                                  setSelectedPurchases((prev) =>
-                                    prev.filter(
-                                      (s) => s.id !== linePurchases[j].id
-                                    )
-                                  );
-                                } else {
-                                  setSelectedPurchases((prev) => [
-                                    ...prev,
-                                    linePurchases[j],
-                                  ]);
-                                }
-                              }}
-                            >
-                              {linePurchases[j].user.name}
-                            </button>
-                          </li>
-                        );
-                      } else {
-                        /** TODO: Check if this else is neccesary */
-                        return (
-                          <li key={j} className="available">
-                            <button
-                            /**
-                                onClick={() => {
-                                  setSelectedLine({
-                                    lineId: i + 1,
-                                    column: line[j]?.column,
-                                  });
-                                  setSelectedUser(
-                                    line[j]?.user ? line[j].user.id : null
-                                  );
-                                }}
-                              */
-                            >
-                              Disponible
-                            </button>
-                          </li>
-                        );
-                      }
-                    })}
-                  </ul>
-                </li>
-              );
-            })}
-        </ul>
+        <LinesMenu
+          lines={lines}
+          linesRef={linesRef}
+          totalLines={totalLines}
+          maxPurchasesPerLine={maxPurchasesPerLine}
+          selectedPurchases={selectedPurchases}
+          setSelectedPurchases={setSelectedPurchases}
+        ></LinesMenu>
+        <Lines lines={lines} linesRef={linesRef}></Lines>
         {selectedPurchases.length > 0 && (
           <div className="confirm-state-change-button-container">
             <button
